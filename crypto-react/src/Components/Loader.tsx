8afb4b975f38e12a755619aea3f27ac8eee65729
Av808080 <108837984+Av808080@users.noreply.github.com>
2023-08-09 01:25:09 +0330

    Last commit

--- crypto-react/src/Components/Loader.tsx
+++ crypto-react/src/Components/Loader.tsx
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
     <>
-     <img src='./Dollar.gif' className='w-40 h-40 block mx-auto mt-8' /> 
+     <img src='./Dollar.gif' className='w-40 h-40 block mx-auto pt-8' /> 
     </>
   )
 }
