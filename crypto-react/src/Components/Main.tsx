5521d4d592c36423294d6d6e8e64cab32d87d8eb
Av808080 <108837984+Av808080@users.noreply.github.com>
2023-08-10 00:39:27 +0330

    Deleting extra code

--- crypto-react/src/Components/Main.tsx
+++ crypto-react/src/Components/Main.tsx
@@ -12,24 +12,18 @@ const Main = ({ data }: Props) => {
         setInput(e.target.value);
     }
 
+    const filteredData = data.filter((item: Data) => item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase()))
+
     return (
         <div className='py-8'>
             <input type="text" placeholder="Search..." value={input} onChange={changeHandler}
                 className="px-3 py-0.5 block mx-auto focus:outline-none ring-1 ring-slate-400 rounded-lg focus:ring-2"
             />
             <div className='w-11/12 xl:w-10/12 2xl:w-9/12 mx-auto rounded-2xl overflow-hidden mt-10 '>
-                {input.length >= 1 ? <>{
-                    data.filter((item: Data) => (item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase()))).map((item: Data) =>
+                {
+                    filteredData.map((item: Data) =>
                         <Card key={item.id} data={NeededInfo(item)} />
                     )
-                }
-                </> : <>
-                    {
-                        data.map((item: Data) =>
-                            <Card key={item.id} data={NeededInfo(item)} />
-                        )
-                    }
-                </>
                 }
             </div>
         </div>
